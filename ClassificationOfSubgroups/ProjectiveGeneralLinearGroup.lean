a9673082936ae3ddb64afb169d5bb0be5157e43c
AlexBrodbelt <apbrodbelt@gmail.com>
2025-02-09 00:53:10 +0000

    fixed typo

--- ClassificationOfSubgroups/ProjectiveGeneralLinearGroup.lean
+++ ClassificationOfSubgroups/ProjectiveGeneralLinearGroup.lean
@@ -130,9 +130,11 @@ namespace ProjectiveGeneralLinearGroup
 
 variable (n : Type u) [DecidableEq n] [Fintype n] (R : Type v) [CommRing R]
 
+open Subgroup
+
 /-- A projective general linear group is the quotient of a special linear group by its center. -/
 abbrev ProjectiveGeneralLinearGroup : Type _ :=
-    GL n R ⧸ Subgroup.center (GL n R)
+    GL n R ⧸ center (GL n R)
 
 
 variable (n : ℕ)
@@ -143,7 +145,7 @@ notation "PGL(" n ", " R ")" => ProjectiveGeneralLinearGroup (Fin n) R
 end ProjectiveGeneralLinearGroup
 
 
-open Matrix LinearMap
+open Matrix LinearMap Subgroup
 
 open scoped MatrixGroups
 
@@ -152,7 +154,7 @@ variable (n : Type u) [DecidableEq n] [Fintype n] (R : Type v) [CommRing R]
 
 /-- A projective special linear group is the quotient of a special linear group by its center. -/
 abbrev ProjectiveSpecialLinearGroup : Type _ :=
-    SpecialLinearGroup n R ⧸ Subgroup.center (SpecialLinearGroup n R)
+    SpecialLinearGroup n R ⧸ center (SpecialLinearGroup n R)
 
 /-- `PSL(n, R)` is the projective special linear group `SL(n, R)/Z(SL(n, R))`. -/
 scoped[MatrixGroups] notation "PSL(" n ", " R ")" => Matrix.ProjectiveSpecialLinearGroup (Fin n) R
@@ -184,25 +186,84 @@ def MonoidHom_PSL_into_PGL : SpecialLinearGroup (Fin n) R ⧸ center (SpecialLin
   map_one' := sorry
   map_mul' := sorry
 
-def PSL_inj_PGL : PSL(n, F) →* PGL(n, F) where
-  toFun := fun S =>
-
-
-    sorry
+def GL_into_PGL {n :Type*} [Fintype n] [DecidableEq n] :
+  GeneralLinearGroup n R →* ProjectiveGeneralLinearGroup n R where
+  toFun := fun G => QuotientGroup.mk' (center (GeneralLinearGroup n R)) G
   map_one' := sorry
   map_mul' := sorry
 
+
+def φ (n : Type*) [Fintype n] [DecidableEq n] (R : Type*) [CommRing R] : SpecialLinearGroup n R →*  ProjectiveGeneralLinearGroup n R where
+  toFun := fun S => QuotientGroup.mk' (center (GeneralLinearGroup n R)) S.toGL
+  map_mul' S₁ S₂ := by simp
+  map_one' := by simp
+
+
+
+#check QuotientGroup.lift
+
+lemma coe  {n : Type*}  [Fintype n] [DecidableEq n] (x : SpecialLinearGroup n R): SpecialLinearGroup.toGL x = (x : Matrix n n R) := rfl
+
+lemma center_SL_le_ker (n : Type*) [Nonempty n] [Fintype n] [DecidableEq n] (R : Type*) [CommRing R] [NeZero (1 : R)]: center (SpecialLinearGroup n R) ≤ (φ n R).ker := by
+  intro x x_mem_center
+  rw [SpecialLinearGroup.mem_center_iff] at x_mem_center
+  obtain ⟨ω, hω, h⟩ := x_mem_center
+  simp [MonoidHom.mem_ker, φ]
+  rw [GeneralLinearGroup.Center.mem_center_general_linear_group_iff]
+  have IsUnit_ω : IsUnit ω := IsUnit.of_pow_eq_one hω Fintype.card_ne_zero
+  use IsUnit_ω.unit
+  ext
+  simp [← GeneralLinearGroup.coe_scalar_matrix, coe, ← h]
+
+instance center_is_normal (n R : Type*) [Nonempty n] [Fintype n] [DecidableEq n] [CommRing R] [NeZero (1 : R)] : Subgroup.Normal (center (SpecialLinearGroup n R)) := normal_of_characteristic (center (SL n R))
+
+instance PGL_is_monoid  (n R : Type*) [Nonempty n] [Fintype n] [DecidableEq n] [CommRing R] [NeZero (1 : R)] : Monoid (ProjectiveGeneralLinearGroup n R) := RightCancelMonoid.toMonoid
+
+def PSL_monoidHom_PGL (n R : Type*) [Nonempty n] [Fintype n] [DecidableEq n] [CommRing R] [NeZero (1 : R)] : ProjectiveSpecialLinearGroup n R →* ProjectiveGeneralLinearGroup n R :=
+  @QuotientGroup.lift (SpecialLinearGroup n R) _ (center (SpecialLinearGroup n R)) (center_is_normal n R) (ProjectiveGeneralLinearGroup n R) (PGL_is_monoid n R) (φ n R) (center_SL_le_ker n R)
+
+open Polynomial
+
+lemma exists_SL (n F : Type*) [Fintype n] [DecidableEq n] [Nonempty n] [Field F] [IsAlgClosed F] (G : GeneralLinearGroup n F) : ∃ c : Fˣ, ∃ S : SpecialLinearGroup n F, c • S.toGL = G := by
+  let P : F[X] := X^(Nat.card n) - C (det G.val)
+  have deg_P_ne_zero : degree P ≠ 0 := by sorry
+  obtain ⟨c, hc⟩ := @IsAlgClosed.exists_root F _ _ P deg_P_ne_zero
+  have c_ne_zero : c ≠ 0 := by sorry
+  have IsUnit_c : IsUnit c := by exact Ne.isUnit c_ne_zero
+  have hc' : ∃ c : Fˣ, c^(Nat.card n) = det G.val := by
+    use IsUnit_c.unit
+    simp [P, sub_eq_zero] at hc
+    simp [hc]
+  obtain ⟨c, hc⟩ := hc'
+  have det_inv_c_G_eq_one : det (c⁻¹ • G).1 = (1 : F) := by
+    simp [← hc, inv_smul_eq_iff, Units.smul_def]
+  use c, ⟨(c⁻¹ • G), det_inv_c_G_eq_one⟩
+  ext
+  simp [coe]
+
+def GL_into_SL (n F : Type*) [Fintype n] [DecidableEq n] [Nonempty n] [Field F] [IsAlgClosed F] : GeneralLinearGroup n F →* SpecialLinearGroup n F where
+  toFun := fun G => sorry
+  map_mul' := sorry
+  map_one' := sorry
+
+def GL_into_PSL (n F : Type*) [Fintype n] [DecidableEq n] [Nonempty n] [Field F] [IsAlgClosed F]  : GeneralLinearGroup n F →* ProjectiveSpecialLinearGroup n F :=
+  by sorry
+
+
+#check MulEquiv.ofBijective
 -- We define the isomorphism between the projective general linear group and the projective special linear group
-def iso : PGL(n,F) ≃* PSL(n,F) where
-  toFun := fun G =>
-    sorry
-  invFun := fun S => sorry
+def iso (n F : Type*) [Fintype n] [DecidableEq n] [Nonempty n] [Field F] [IsAlgClosed F] : ProjectiveGeneralLinearGroup n F ≃* ProjectiveSpecialLinearGroup n F where
+  toFun := sorry
+  invFun := PSL_monoidHom_PGL n F
   left_inv := sorry
   right_inv := sorry
   map_mul' := sorry
 
-#leansearch "representative of equivalence class?"
+#leansearch "center of special linear group?"
 
+
+
+#check QuotientGroup.map
 
 end Isomorphism
 
