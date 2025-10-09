d8c5cecf366bcd4b9520adfefd68bf191abc48f4
Rado Kirov <rkirov@gmail.com>
2025-10-08 23:35:32 -0700

    clean up in Ch6-S1

--- ClassificationOfSubgroups/Ch6_MaximalAbelianSubgroupClassEquation/S1_ElementaryAbelian.lean
+++ ClassificationOfSubgroups/Ch6_MaximalAbelianSubgroupClassEquation/S1_ElementaryAbelian.lean
@@ -2,20 +2,10 @@ import Mathlib.Data.Nat.Factorization.PrimePow
 import Mathlib.GroupTheory.PGroup
 import Mathlib.Order.CompletePartialOrder
 
-
-set_option linter.style.longLine true
-set_option autoImplicit false
 set_option maxHeartbeats 0
 set_option synthInstance.maxHeartbeats 0
 
 open Subgroup
-
-
-
-structure ElementaryAbelian (p : ℕ) (G : Type*) [Group G] extends Subgroup G where
-  is_comm : IsCommutative toSubgroup
-  orderOf_eq : ∀ h : toSubgroup, h ≠ 1 → orderOf h = p
-
 
 def IsElementaryAbelian {G : Type*} [Group G] (p : ℕ) (H : Subgroup G) : Prop :=
   IsCommutative H ∧ ∀ h : H, h ≠ 1 → orderOf h = p
@@ -24,33 +14,33 @@ namespace IsElementaryAbelian
 
 lemma dvd_card {G : Type*} [Group G] {p : ℕ} {H : Subgroup G}
   [Finite H] (hH : IsElementaryAbelian p H) (bot_lt_H: ⊥ < H) : p ∣ (Nat.card H) := by
-  simp [@SetLike.lt_iff_le_and_exists] at bot_lt_H
+  simp [SetLike.lt_iff_le_and_exists] at bot_lt_H
   obtain ⟨h, h_in_H, h_ne_one⟩ := bot_lt_H
-  have order_eq_p : @orderOf H _ ⟨h, h_in_H⟩ = p := by
+  have order_eq_p : orderOf (⟨h, h_in_H⟩ : H) = p := by
     apply hH.right ⟨h, h_in_H⟩
     simp [h_ne_one]
   rw [← order_eq_p]
   let inst : Fintype (H :) := Fintype.ofFinite ↥H
-  have order_dvd_card := @orderOf_dvd_card H _ _ ⟨h, h_in_H⟩
+  have order_dvd_card := orderOf_dvd_card (x := (⟨h, h_in_H⟩ : H))
   simp at order_dvd_card ⊢
   exact order_dvd_card
-
 
 lemma primeFac_eq {G : Type*} [Group G] (p : ℕ)
   (hp : Nat.Prime p) (H : Subgroup G) [Finite H] (hH : IsElementaryAbelian p H) (bot_lt_H : ⊥ < H):
   Nat.primeFactors (Nat.card H) = {p} := by
-  rw [@Finset.Subset.antisymm_iff]
+  rw [Finset.Subset.antisymm_iff]
   constructor
   -- Suppose the set of prime factors is not contained in {p}
   · by_contra! h
-    rw [@Finset.not_subset] at h
+    rw [Finset.not_subset] at h
     obtain ⟨q, hq, q_ne_p⟩ := h
     simp [← ne_eq] at q_ne_p
     rw [Nat.mem_primeFactors] at hq
     obtain ⟨hq, q_dvd_card, -⟩ := hq
     let Fintype_H : Fintype H := Fintype.ofFinite ↥H
     simp at q_dvd_card
-    obtain ⟨x, order_eq_q⟩ := @exists_prime_orderOf_dvd_card H _ _ q ({out := hq}) q_dvd_card
+    have hq' : Fact (Nat.Prime q) := ⟨hq⟩
+    obtain ⟨x, order_eq_q⟩ := exists_prime_orderOf_dvd_card q q_dvd_card
     have q_ne_one : q ≠ 1 := Nat.Prime.ne_one hq
     have x_ne_one : x ≠ 1 := by
       intro h
@@ -63,15 +53,12 @@ lemma primeFac_eq {G : Type*} [Group G] (p : ℕ)
     exact
       ⟨hp, dvd_card hH bot_lt_H, Nat.ne_zero_iff_zero_lt.mpr Nat.card_pos⟩
 
-
-
 lemma IsPGroup {G : Type*} [hG : Group G] (p : ℕ) (hp : Nat.Prime p)
   (H : Subgroup G) [hH₀ : Finite H] (hH : IsElementaryAbelian p H) (bot_lt_H : ⊥ < H):
   IsPGroup p H := by
-  let hp' : Fact (Nat.Prime p) := { out := hp }
+  have hp' : Fact (Nat.Prime p) := ⟨hp⟩
   rw [IsPGroup.iff_card]
-  have : Nat.primeFactors (Nat.card (H :)) = {p} :=
-    @primeFac_eq G _ p hp H _ hH bot_lt_H
+  have : Nat.primeFactors (Nat.card (H :)) = {p} := primeFac_eq p hp H hH bot_lt_H
   have card_primeFac_eq_one : (Nat.card ↥H).primeFactors.card = 1 := this ▸ rfl
   have card_eq_isPrimePow :=
     (@isPrimePow_iff_card_primeFactors_eq_one (Nat.card (H :))).mpr card_primeFac_eq_one
@@ -84,7 +71,6 @@ lemma IsPGroup {G : Type*} [hG : Group G] (p : ℕ) (hp : Nat.Prime p)
     exact ⟨hp', p'_dvd_card, Nat.ne_zero_iff_zero_lt.mpr Nat.card_pos⟩
   simp [this] at p_eq_p'
   use k, p_eq_p'▸ card_eq.symm
-
 
 lemma subgroupOf {G : Type*} [Group G]
   (H K : Subgroup G) {p : ℕ} [Fact (Nat.Prime p)] (hH : IsElementaryAbelian p H) :
@@ -105,5 +91,3 @@ lemma subgroupOf {G : Type*} [Group G]
     simp [← order_of_eq_p']
 
 end IsElementaryAbelian
-
-#min_imports
